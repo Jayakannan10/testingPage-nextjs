962f21e941b57431fbdde8f762a3964b27b2a669
jayakannanmavencart <jayakannan@mavencart.com>
2025-04-21 15:32:07 +0530

    use client is added for the Hero.jsx file

--- components/home/Hero.tsx
+++ components/home/Hero.tsx
@@ -1,5 +1,8 @@
+'use client';
+
+
+import {useEffect} from 'react';
 import { ArrowRight } from 'lucide-react';
-import {useEffect} from 'react'
 import Link from 'next/link';
 
 export default function Hero() {
